32f3e3cb59f8134d793662525177761b7f014059
dongha24 <dongha24@dongha24-All-Series>
2019-06-20 17:44:47 +0900

    주석 문제임 ascii

--- test/src/test/java/com/gominc/test/TestingSelenium.java
+++ test/src/test/java/com/gominc/test/TestingSelenium.java
@@ -11,15 +11,14 @@ public class TestingSelenium {
 	private final static String BASE_URL = "http://dpgs.gompay.net:8080/test"; 
 	
 	@BeforeClass 
-	public static void openBrowser() {//웹드라이버를 세팅 
-	System.setProperty("webdriver.chrome.driver", "src/test/driver/chromedriver"); // ChromeDriver set
-	ChromeDriver = new ChromeDriver(); // Driver 생성	
+	public static void openBrowser() { 
+	System.setProperty("webdriver.chrome.driver", "src/test/driver/chromedriver");
+	ChromeDriver = new ChromeDriver();	
 	} 
 
 	@Test 
 	public void test() { 
-		ChromeDriver.get(BASE_URL); // URL로 접속하기
-//        Assert.assertThat(ChromeDriver.getTitle(),is("URL의 Title")); // Title 확인 작업
+		ChromeDriver.get(BASE_URL);        
 	} 
 	
 	@AfterClass 
