6466215dfd4482c7f4b353c3398a85c735d9bd1b
dongha24 <dongha24@dongha24-All-Series>
2019-06-20 18:28:45 +0900

    a

--- test/src/test/java/com/gominc/test/TestingSelenium.java
+++ test/src/test/java/com/gominc/test/TestingSelenium.java
@@ -7,5 +7,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestingSelenium {
 
+	private static ChromeDriver ChromeDriver; 
+//	private final static String BASE_URL = "http://dpgs.gompay.net:8080/test";
+	private final static String BASE_URL = "http://naver.com";
+	
+	@BeforeClass 
+	public static void openBrowser() { 
+	System.setProperty("webdriver.chrome.driver", "src/test/driver/chromedriver");
+	ChromeDriver = new ChromeDriver();	
+	} 
 
+	@Test 
+	public void test() { 
+		ChromeDriver.get(BASE_URL);        
+	} 
+	
+	@AfterClass 
+	public static void closeBrowser() { 
+		ChromeDriver.quit(); 
+	}		
 }
