d85f987eb65400bc5406396c290d1dfa905fc1b3
dongha24 <dongha24@dongha24-All-Series>
2019-06-21 17:59:36 +0900

    ac

--- test/src/test/java/com/gominc/test/TestingSelenium.java
+++ test/src/test/java/com/gominc/test/TestingSelenium.java
@@ -10,7 +10,7 @@ public class TestingSelenium {
 
 	private static ChromeDriver ChromeDriver; 
 //	private final static String BASE_URL = "http://dpgs.gompay.net:8080/test";
-	private final static String BASE_URL = "";
+	private final static String BASE_URL = "http://naver.com";
 	
 	@BeforeClass 
 	public static void openBrowser() {		
